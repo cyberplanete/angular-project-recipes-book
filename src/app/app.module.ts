56f9478fe8410e1fb8f06501ff779a891206e610
Pascal <pascal@pascal-desktop>
2018-06-26 22:50:25 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { LivreRecettesComponent} from './livre-recettes/livre.recettes.component
 import { HeaderComponent } from './header/header.component';
 import { ListeRecettesComponent } from './livre-recettes/liste-recettes/liste-recettes.component';
 import { DetailRecetteComponent } from './livre-recettes/detail-recette/detail-recette.component';
-import { RecetteComponent } from './livre-recettes/liste-recettes/element-liste-recette/element.liste.recette.component';
+import { RecetteComponent } from './livre-recettes/element-liste-recette/element.liste.recette.component';
 import { ListeCoursesComponent } from './liste-courses/liste-courses.component';
 import { ListeCoursesEditeurComponent } from './liste-courses/liste-courses-editeur/liste-courses-editeur.component';
 
