2bec9c319b5122efa1294af74cfa5363c91b2a16
Pascal <pascal@pascal-desktop>
2018-06-26 14:30:12 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 
-import { RecettesComponent} from './recettes/recettes.component';
+import { RecettesComponent} from './livre-recettes/recettes.component';
 import { HeaderComponent } from './header/header.component';
-import { ListeRecettesComponent } from './recettes/liste-recettes/liste-recettes.component';
-import { DetailRecetteComponent } from './recettes/detail-recette/detail-recette.component';
-import { RecetteComponent } from './recettes/liste-recettes/element-liste-recette/recette.component';
+import { ListeRecettesComponent } from './livre-recettes/liste-recettes/liste-recettes.component';
+import { DetailRecetteComponent } from './livre-recettes/detail-recette/detail-recette.component';
+import { RecetteComponent } from './livre-recettes/liste-recettes/element-liste-recette/recette.component';
 import { ListeCoursesComponent } from './liste-courses/liste-courses.component';
 import { ListeCoursesEditeurComponent } from './liste-courses/liste-courses-editeur/liste-courses-editeur.component';
 
