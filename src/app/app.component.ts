04cf341c288041d997d2b91fbe65cd293b3d5d64
Pascal <pascal@pascal-desktop>
2018-07-28 17:54:52 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  chargeCaracteristique = 'menuRecettes';
+
+  surNavigation(caracteristique: string) {
+
+    this.chargeCaracteristique = caracteristique;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({ selector : 'app-header', templateUrl : 'header.component.html', styleUrls : ['header.component.css'] })
 export class HeaderComponent {
+// Reception du menu header l'element selectionné . @Output renvoi l'instance vers app.component.html
+  @Output() caracteristiqueSelectionne = new EventEmitter<String>();
+
+  siSelectionne (caracteristique: String) {
+
+    this.caracteristiqueSelectionne.emit(caracteristique);
+
+  }
 
 
 }
